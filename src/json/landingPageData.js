fbf3b35f239043a6b532bc2ea7868ee0101590cb
Rajan Patel0 <rajanpatel.ias@gmail.com>
2024-12-27 19:22:50 +0530

    first commit

---
+++ src/json/landingPageData.js
@@ -0,0 +1,286 @@
+/* eslint-disable import/extensions */
+
+// Services Img Imports
+import Web from '../assets/images/Services/Web.png';
+import Mobile from '../assets/images/Services/Mobile.png';
+import SAAS from '../assets/images/Services/saas.png';
+import UIUX from '../assets/images/Services/Design.png';
+
+// Portfolio Img Imports
+import Iztend from '../assets/images/Portfolio/Iztend.png';
+import RDTrack from '../assets/images/Portfolio/RdTrack.png';
+import Profile from '../assets/images/Portfolio/Profile.png';
+import Recruiting from '../assets/images/Portfolio/Recruiting.png';
+import Stream from '../assets/images/Portfolio/Stream.png';
+import Freelance from '../assets/images/Portfolio/Freelance.png';
+import Aura from '../assets/images/Portfolio/Aura.png';
+import Surtido from '../assets/images/Portfolio/Surtido.png';
+import ManagementApp from '../assets/images/Portfolio/ManagementApp.png';
+
+// Advantages
+import Communicative from '../assets/images/Advantages/Communicative.png';
+import Collaborative from '../assets/images/Advantages/Collaborative.png';
+import Management from '../assets/images/Advantages/Management.png';
+import Favorite from '../assets/images/Advantages/Favorite.png';
+
+// Testimonials
+import Sasha from '../assets/images/Testimonials/nt.jpg';
+import Reiner from '../assets/images/Testimonials/ge.jpg';
+import Kruger from '../assets/images/Testimonials/Kruger.jpg';
+
+// TeamMembers
+import CEO from '../assets/images/TeamMembers/CEO.jpg';
+import HRD from '../assets/images/TeamMembers/HRD.jpg';
+import Finance from '../assets/images/TeamMembers/Finance.jpg';
+import ProjectManager from '../assets/images/TeamMembers/Project-manager.jpg';
+import Frontend1 from '../assets/images/TeamMembers/Frontend1.jpg';
+import Frontend2 from '../assets/images/TeamMembers/Frontend2.jpg';
+import Backend1 from '../assets/images/TeamMembers/Backend1.jpg';
+import Backend2 from '../assets/images/TeamMembers/Backend2.jpg';
+import Mobile1 from '../assets/images/TeamMembers/Mobile1.jpg';
+import Mobile2 from '../assets/images/TeamMembers/Mobile2.jpg';
+import UIUX1 from '../assets/images/TeamMembers/UIUX1.jpg';
+import UIUX2 from '../assets/images/TeamMembers/UIUX2.jpg';
+
+export const Services = [
+  {
+    title: 'Web Development',
+    imageUrl: Web,
+    animation: 'left',
+  },
+  {
+    title: 'Mobile Development',
+    imageUrl: Mobile,
+    animation: 'up',
+  },
+  {
+    title: 'Saas & Tools',
+    imageUrl: SAAS,
+    animation: 'down',
+  },
+  {
+    title: 'UI/UX Design',
+    imageUrl: UIUX,
+    animation: 'right',
+  },
+];
+
+export const Portfolios = [
+  {
+    id: 'asd1293fdjicfh6',
+    title: 'Iztend',
+    imageUrl: Iztend,
+    type: 'Website',
+    detail: 'This E-commerce platform is designed to provide a seamless shopping experience for users, integrating advanced technologies to enhance functionality and performance. Project includes Frontend: Developed using React, offering a responsive and interactive user interface, Backend: Built with Node.js and Express.js, ensuring a scalable and secure architecture, Database: Powered by MongoDB for efficient and flexible data management, Payments: Integrated with Razorpay for secure and hassle-free payment processing, Caching: Utilized Redis to improve application performance through caching and session management, Media Management: Leveraged Cloudinary for optimized image and file storage solutions.',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://e-comm-tblb.onrender.com',
+  },
+  {
+    id: 'asd1293khftvhj7',
+    title: 'Device Tracker',
+    imageUrl: RDTrack,
+    type: 'Mobile',
+    detail: 'Real-Time Device Tracking App – Bringing Real-Time Collaboration to Life. The app can track multiple devices in real-time, showcasing their locations dynamically on a map, making it perfect for collaboration, monitoring, or logistics.',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://real-time-device-tracker-9m2x.onrender.com',
+  },
+  {
+    id: 'asd1293wsaweui9',
+    title: 'Personals',
+    imageUrl: Profile,
+    type: 'Website',
+    detail: ' This portfolio website showcases my journey, skills in the MERN stack, and recent projects. also with complete responsiveness Each project reflects my commitment to creating impactful, user-centered solutions',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://rajanpatel0.github.io/My-Portfolio',
+  },
+  {
+    id: 'asd1293ufgdfgs4',
+    title: 'Aura',
+    imageUrl: Aura,
+    type: 'Website',
+    detail: 'A Music Player Website that creates personalized playlist in seconds',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15176338-Aura-Website-Main-Page',
+  },
+  {
+    id: 'asd1293uhjkhkjh2',
+    title: 'Stream+',
+    imageUrl: Stream,
+    type: 'Mobile Apps',
+    detail: 'A Video Player app that streams personalized playlist of series for you',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15276430-Stream',
+  },
+  {
+    id: 'asd1293ulskmnb5',
+    title: 'Surtido Rico',
+    imageUrl: Surtido,
+    type: 'Website',
+    detail: 'For Describing trendy Fashion',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15173118-Surtido-Rico',
+  },
+  {
+    id: 'asd1293uasdads1',
+    title: 'Recruiting App',
+    imageUrl: Recruiting,
+    type: 'Mobile Apps',
+    detail: 'Different Roles Recruiting App',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15164950-Recruiting-app',
+  },
+  {
+    id: 'asd1293ulkmnbj6',
+    title: 'Courses Management',
+    imageUrl: ManagementApp,
+    type: 'Website',
+    detail: 'Course Managent In Simple Comprehense way lead Good Way',
+    responsibility: [
+      'Web Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15197890-Courses-Management-and-Promoting-Web-App',
+  },
+  {
+    id: 'asd1293uvbvcbbd3',
+    title: 'Freelance',
+    imageUrl: Freelance,
+    type: 'Mobile Apps',
+    detail: 'A prototye showcasing the flow of work in freelance world.',
+    responsibility: [
+      'Mobile Development',
+      'UI/UX Design',
+    ],
+    credit: 'https://dribbble.com/shots/15223131-Freelance-Mobile-App-Concept',
+  },
+];
+
+export const Advantages = [
+  [{
+    title: 'Communicative',
+    description: 'We communicate our project ideas and progress to make it clear.',
+    imageUrl: Communicative,
+  },
+  {
+    title: 'Management',
+    description: 'We manage our project properly to make our project done well.',
+    imageUrl: Management,
+  }],
+  [{
+    title: 'Collaborative​',
+    description: 'Our team are very collaborative to make our project done well.',
+    imageUrl: Collaborative,
+  },
+  {
+    title: 'Favorite',
+    description: "We've did so many project and all of our client love it.",
+    imageUrl: Favorite,
+  }],
+];
+export const Testimonials = [
+  {
+    id: 1,
+    name: 'Sumanth',
+    company: 'Executor, Nature Touch Nutritions',
+    testimoni: 'Thanks for AIntend Solutions, you guys are the best! Keep up the great work!',
+    imageUrl: Sasha,
+  },
+  {
+    id: 2,
+    name: 'Sanjeev Kumar',
+    company: 'Owner, HP Worlds',
+    testimoni: 'I just wanted to let you know that it’s been great working with AIntend Solutions.',
+    imageUrl: Kruger,
+  },
+  {
+    id: 3,
+    name: 'Rohit',
+    company: 'Assistent, Gahir Electronics',
+    testimoni: 'AIntend Solutions is so great. Thank you so much for a job well done.',
+    imageUrl: Reiner,
+  },
+];
+
+export const TeamMembers = [
+  {
+    name: 'Rajan Patel',
+    position: 'Founder, CEO',
+    imageUrl: CEO,
+  },
+  {
+    name: 'Pauline Sydney',
+    position: 'HRD',
+    imageUrl: HRD,
+  },
+  {
+    name: 'Granger Watterson',
+    position: 'Finance',
+    imageUrl: Finance,
+  },
+  {
+    name: 'Tom Jimmy',
+    position: 'Project Manager',
+    imageUrl: ProjectManager,
+  },
+  {
+    name: 'Jim Hendrix',
+    position: 'Front-end Developer',
+    imageUrl: Frontend1,
+  },
+  {
+    name: 'Calvin Max',
+    position: 'Front-end Developer',
+    imageUrl: Frontend2,
+  },
+  {
+    name: 'Hawkins Jim',
+    position: 'Back-end Developer',
+    imageUrl: Backend1,
+  },
+  {
+    name: 'Don Bizaro',
+    position: 'Back-end Developer',
+    imageUrl: Backend2,
+  },
+  {
+    name: 'Bill Markinson',
+    position: 'Mobile Developer',
+    imageUrl: Mobile1,
+  },
+  {
+    name: 'Igor Kavarov',
+    position: 'Mobile Developer',
+    imageUrl: Mobile2,
+  },
+  {
+    name: 'Freddie Curl',
+    position: 'UI/UX Designer',
+    imageUrl: UIUX2,
+  },
+  {
+    name: 'Monica Lovegood',
+    position: 'UI/UX Designer',
+    imageUrl: UIUX1,
+  },
+];
